265843829ebf1ccbf009eaf9fe470e20f64b1791
Folau Kaveinga <folaukaveinga@gmail.com>
2024-08-14 23:34:05 -0600

    setup

---
+++ src/main/java/com/folauetau/retrofit/SpringbootWithRetrofitApplication.java
@@ -0,0 +1,13 @@
+package com.folauetau.retrofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRetrofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithRetrofitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folauetau/retrofit/SpringbootWithRetrofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folauetau.retrofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRetrofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
